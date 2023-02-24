f75439bca3a93e85c57083be0d1f56d3fd88b5f2
jsyeh <jsyeh@mail.mcu.edu.tw>
2023-02-24 15:26:23 +0800

    修改目錄、新增Week01

---
+++ week01/week01-4.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char line[40];
+    int total = 0;
+    scanf("%s",line);
+    int N = stelen(line);
+    for(int i=0;i<N;i++)
+    {
+        if(line[i] == '2');
+            ans++;
+    }
+    printf("%d\n",ans);
+    total += ans;
+}
